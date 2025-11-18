d1bf81973ca9a6bcb503adc95034b95be7d267d7
Gurshaan Daula <gurshaandaula@gmail.com>
2025-11-17 16:34:26 -0800

    please mongo just work

--- config/sessionStore.js
+++ config/sessionStore.js
@@ -1,27 +1,23 @@
-require("dotenv").config();
-const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
-// Create session middleware
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI,
-        mongoOptions: {
-            serverSelectionTimeoutMS: 10000,
-            ssl: true,
-            tlsAllowInvalidCertificates: true
-        },
-        crypto: { secret: process.env.SESSION_SECRET }
-    }),
     cookie: {
         maxAge: 1000 * 60 * 60, // 1 hour
-        httpOnly: true,
-        sameSite: "lax",
-        secure: false // change to true if using HTTPS domain
-    }
+        secure: process.env.NODE_ENV === "production"
+    },
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        crypto: {
+            secret: process.env.SESSION_SECRET
+        },
+        autoRemove: "interval",
+        autoRemoveInterval: 10,
+        tls: true,
+        tlsAllowInvalidCertificates: false
+    })
 });
 
 module.exports = sessionMiddleware;
