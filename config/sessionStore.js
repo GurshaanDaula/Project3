c5835d78c4b9e58d1f0eadee5def4e2d43380adb
Gurshaan Daula <gurshaandaula@gmail.com>
2025-11-17 16:07:32 -0800

    render again

--- config/sessionStore.js
+++ config/sessionStore.js
@@ -2,18 +2,15 @@ require("dotenv").config();
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
-const sessionMiddleware = session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60, // 1 hour
-        httpOnly: true
+store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    mongoOptions: {
+        serverSelectionTimeoutMS: 10000,
+        ssl: true,
+        tlsAllowInvalidCertificates: true
     },
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI,
-        crypto: { secret: process.env.SESSION_SECRET }
-    })
-});
+    crypto: { secret: process.env.SESSION_SECRET }
+})
+
 
 module.exports = sessionMiddleware;
